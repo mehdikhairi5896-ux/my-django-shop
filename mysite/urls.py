5c883af8ada3abaedb225b64ad8ae7611f6e90bf
Mehdi Khairi <mehdi.khairi.5896@gmail.com>
2026-07-18 20:43:59 +0330

    Remove backup file

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, shop, product_detail
+from .views import home
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
-    path('shop/', shop),
-    path('shop/<int:product_id>/', product_detail),
+    path('shop/', include('shop.urls')),
     path('cart/', include('cart.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
--- mysite/views.py
+++ mysite/views.py
@@ -1,22 +1,5 @@
 from django.shortcuts import render
-from shop.models import Product
 
 def home(request):
     return render(request, 'index.html', {'test_message': 'صفحه خانه درست است'})
 
-def shop(request):
-    search = request.GET.get('search')
-
-    if search:
-        products = Product.objects.filter(name__icontains=search)
-    else:
-        products = Product.objects.all()
-
-    return render(request, 'shop.html', {
-        'products': products,
-        'search': search,
-    })
-
-def product_detail(request, product_id):
-    product = Product.objects.get(id=product_id)
-    return render(request, 'product_detail.html', {'product': product})
