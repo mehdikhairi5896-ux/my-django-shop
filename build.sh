d7985262e8f96c1e9b4c11c73cd48101c0b33fdb
Mehdi Khairi <mehdi.khairi.5896@gmail.com>
2026-07-24 23:35:23 +0330

    fix render admin login

--- build.sh
+++ build.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python create_admin.py
+set -o errexit
+
+python manage.py collectstatic --no-input
+
+python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth.models import User
+
+u, created = User.objects.get_or_create(username='Admin2')
+
+u.set_password('12345678')
+u.is_staff = True
+u.is_superuser = True
+u.save()
+
+print('Admin2 ready')
+"
