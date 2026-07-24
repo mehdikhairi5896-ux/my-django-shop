dbddef09d54f5897c49a9a1b6394fd0b76721478
Mehdi Khairi <mehdi.khairi.5896@gmail.com>
2026-07-24 23:46:19 +0330

    create new render admin

--- build.sh
+++ build.sh
@@ -8,12 +8,13 @@ python manage.py migrate
 python manage.py shell -c "
 from django.contrib.auth.models import User
 
-u, created = User.objects.get_or_create(username='Admin2')
+User.objects.filter(username='renderadmin').delete()
 
-u.set_password('12345678')
-u.is_staff = True
-u.is_superuser = True
-u.save()
+u = User.objects.create_superuser(
+    username='renderadmin',
+    email='renderadmin@example.com',
+    password='12345678'
+)
 
-print('Admin2 ready')
+print('NEW ADMIN CREATED:', u.username)
 "
