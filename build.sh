b5a0b74553f27ecdcf2a2cb8102b1f060b26e11f
Mehdi Khairi <mehdi.khairi.5896@gmail.com>
2026-09-10 00:03:43 +0330

    Secure renderadmin password

--- build.sh
+++ build.sh
@@ -6,14 +6,15 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 python manage.py shell -c "
-from django.contrib.auth.models import User
 
+from django.contrib.auth.models import User
+import os
 User.objects.filter(username='renderadmin').delete()
 
 u = User.objects.create_superuser(
     username='renderadmin',
     email='renderadmin@example.com',
-    password='12345678'
+    password=os.environ.get('RENDER_ADMIN_PASSWORD')
 )
 
 print('NEW ADMIN CREATED:', u.username)
