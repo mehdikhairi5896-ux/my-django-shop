2456c73c35dbd588c652086adca31e0357ad3f29
Mehdi Khairi <mehdi.khairi.5896@gmail.com>
2026-09-10 01:36:56 +0330

    Remove duplicate Product import

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from shop.models import Product
-from shop.models import Product
 from .models import CartItem, Order, OrderItem, OrderPayment
 from .forms import OrderForm
 from django.db import transaction
